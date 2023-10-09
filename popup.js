9da47e5f721a9ce47ee9b2a0492ddd5d911d65bc
Balazs <roboticpandadev@gmail.com>
2023-10-09 17:57:00 +0200

    Extension popup initial layout

--- popup.js
+++ popup.js
@@ -1,6 +1,88 @@
-document.getElementById('start').addEventListener('click', (e) => {
+/*document.getElementById('start').addEventListener('click', (e) => {
     browser.runtime.sendMessage({command: "startTimer", seconds: 10, ruleId: 1337})
 })
 document.getElementById('stop').addEventListener('click', (e) => {
     browser.runtime.sendMessage({command: "stopTimer"})
+})*/
+
+/* Populate the list of rules from localStorage */
+document.addEventListener('DOMContentLoaded', function() {
+    const a = document.getElementById('ruleset-container');
+    if (a) {
+        testRules.sort((a, b) => a.priority - b.priority);
+        for (rule of testRules) {
+            a.innerHTML += '<div class="row row-active row-hover align-items-center border-secondary border-top border-opacity-25" id="swt-rule-' + rule.id + '">' +
+            '<div class="col sm fs-3 text-end"><span class="settings-icon" role="button">' + (rule.priority > 0 ? '↑' : '') + '</span></div>' +
+            '<div class="col sm fs-3 text-start"><span class="settings-icon" role="button">' + (rule.priority < testRules.length-1 ? '↓' : '') + '</span></div>' +
+            '<div class="col-4 text-truncate">' + rule.name + '</div>' +
+            '<div class="col-2 text-center" id="swt-timeleft-'+ rule.id +'">' + formatTime(rule.timeLeftSeconds) + '</div>' +
+            '<div class="col-4 text-center"> <span class="d-inline p-2 settings-icon"><input role=button class="form-check-input" type="checkbox" value=""' + (rule.isEnabled ? ' checked' : '') + '></span>' +
+            '<span class="d-inline p-1 settings-icon" role="button">' + '⚙' + '</span>' +
+            '<span class="d-inline p-1 settings-icon" role="button">' + '🗑️' + '</span></div>' +
+            '</div>'
+        }
+    }
 })
+
+var testRules = [
+    { 
+        'id': 152,
+        'name': 'Google',
+        'blockString': 'google.com',
+        'exceptString': 'mail.google.com, accounts.google.com',
+        'timeAllocatedMinutes': 180,
+        'timeLeftSeconds': 7900,
+        'isEnabled': true,
+        'lastReset': new Date('2023-10-09'),
+        'priority': 0
+    },
+    { 
+        'id': 155,
+        'name': 'Reddit',
+        'blockString': 'reddit.com',
+        'exceptString': 'r/learnprogramming',
+        'timeAllocatedMinutes': 20,
+        'timeLeftSeconds': 15,
+        'isEnabled': true,
+        'lastReset': new Date('2023-10-08'),
+        'priority': 1
+    },
+    { 
+        'id': 170,
+        'name': 'Social Media',
+        'blockString': 'reddit.com, facebook.com, twitter.com, tiktok.com',
+        'exceptString': '',
+        'timeAllocatedMinutes': 30,
+        'timeLeftSeconds': 1522,
+        'isEnabled': true,
+        'lastReset': new Date('2023-10-09'),
+        'priority': 3
+    },
+    { 
+        'id': 252,
+        'name': 'edddddddddddddddddddddd dddddddddddddX',
+        'blockString': 'edx.com',
+        'exceptString': '',
+        'timeAllocatedMinutes': 1,
+        'timeLeftSeconds': 10,
+        'isEnabled': false,
+        'lastReset': new Date('2023-10-05'),
+        'priority': 2
+    },
+]
+
+/* Formats seconds to mm:ss format */
+function formatTime(seconds) {
+    if (seconds) {
+        let minutes = Math.floor(seconds / 60);
+        seconds = seconds % 60;
+        return (
+            (minutes < 10 ? '0' + minutes : minutes) + ':' +
+            (seconds < 10 ? '0' + seconds : seconds)
+        );
+    }
+    else {
+        return '00:00';
+    }
+
+}
